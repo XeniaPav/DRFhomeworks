166c25673c51074391a93f39afb868ceb02f8bf8
Pavlukhina Ksenia <kseny100297@yandex.ru>
2024-09-30 18:51:07 +0500

    ДЗ 2, urls

---
+++ users/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from users.apps import UsersConfig
+from users.views import UserViewSet, PaymentListAPIView
+from rest_framework.routers import SimpleRouter
+
+
+app_name = UsersConfig.name
+
+router = SimpleRouter()
+router.register("users", UserViewSet)
+
+
+urlpatterns = [
+    path("payment_list/", PaymentListAPIView.as_view(), name="payment_list"),
+]
+
+urlpatterns += router.urls
